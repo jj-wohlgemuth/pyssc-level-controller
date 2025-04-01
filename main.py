9c3f35ff389012d1e8b08b514b6097b7ae73adf2
jj-wohlgemuth <52459869+jj-wohlgemuth@users.noreply.github.com>
2025-04-01 09:35:56 +0200

    Update main.py
    fixed a typo in module import

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
 from pickle import TRUE
 from RPi import GPIO
-from VolumeControl import VolumeControl
+from volume_control import VolumeControl
 import time
 
 CLOCKPIN = 5
 DATAPIN = 6
 SWITCHPIN = 13
 VOLUME_STEP_DB = 1
-SEND_TIME_S = .5
+SEND_TIME_S = 0.5
 LIGHT_TIME_S = 5
 
 
@@ -25,12 +25,12 @@ lc = LCD()
 GPIO.setmode(GPIO.BCM)
 found = False
 while not found:
-    if os.path.exists('setup.json'):
-        found_setup = ssc.Ssc_device_setup()
-        found_setup.from_json('setup.json')
+    if os.path.exists("setup.json"):
+        found_setup = ssc.Ssc_device_setup()˜
+        found_setup.from_json("setup.json")
         found = TRUE
     else:
-        lc.text('setup.json not found', 1)
+        lc.text("setup.json not found", 1)
 network_ready = False
 while not network_ready:
     try:
@@ -38,12 +38,19 @@ while not network_ready:
         network_ready = True
     except Exception as e:
         time.sleep(5)
-        lc.text('Network error: ', 1)
+        lc.text("Network error: ", 1)
         lc.text(str(e), 2)
 
-vc = VolumeControl(CLOCKPIN, DATAPIN, SWITCHPIN,
-                   found_setup, VOLUME_STEP_DB, SEND_TIME_S,
-                   LIGHT_TIME_S, lc)
+vc = VolumeControl(
+    CLOCKPIN,
+    DATAPIN,
+    SWITCHPIN,
+    found_setup,
+    VOLUME_STEP_DB,
+    SEND_TIME_S,
+    LIGHT_TIME_S,
+    lc,
+)
 try:
     vc.start()
     pause()
